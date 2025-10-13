43e4c54bbdd70643c9c355559544fa50b0c48449
Isha Chib <ishachib641@gmail.com>
2025-10-13 18:59:02 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,34 @@
+//number 
+2 + 3;
+alert (2+3);
+
+// string
+var myName ="Isha chib.";
+alert(myName);
+
+// boolean
+var isPass = true;
+alert(isPass);
+isPass = false;
+alert(isPass);
+// null
+var myVar = null;
+alert(myVar);
+// undefined
+var myVar2;
+alert(myVar2);
+myVar2 = 5;
+alert(myVar2);
+// object
+var student = {
+    name : "Isha chib",
+    age : 17,
+    isPass : true,
+};
+alert(student);
+alert(student.name);
+alert(student.age);
+alert(student.isPass);
+student.marks = 89;
+alert(student.marks);
+
